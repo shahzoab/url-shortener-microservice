5676d1cc27e47b5fc3beb7921bb8e282d9656d7f
shahzoab <shahzoab@gmail.com>
2023-11-12 17:24:58 +0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,24 +1,94 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+
+// Connect to database
+let mongoose = require("mongoose");
+mongoose.connect(process.env["MONGO_URI"], {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// init express
+const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (_req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// Create Url schema
+let UrlShema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url: {
+    type: Number,
+    required: true
+  }
 });
 
-app.listen(port, function() {
+// Create Url model
+let Url = mongoose.model("Url", UrlShema);
+
+// Parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// API endpoint to create shorturl
+app.post("/api/shorturl", async function (req, res) {
+  const count = await Url.countDocuments();
+  const original_url = req.body.url;
+  const short_url = count + 1;
+
+  // Check if url is valid
+  const pattern = new RegExp(
+    "^([a-zA-Z]+:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR IP (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$", // fragment locator
+    "i"
+  );
+  if (!pattern.test(original_url)) {
+    res.json({ error: "invalid url" });
+    return;
+  }
+
+  // Check if original_url is already in db then return it
+  const exist = await Url.findOne({ original_url }).select("-_id -__v");
+  if (exist) {
+    res.json(exist);
+    return;
+  }
+  // else create new document in db and return it
+  const newUrl = new Url({ original_url, short_url });
+  newUrl
+    .save()
+    .then(data => {
+      res.json({ original_url: data.original_url, short_url: data.short_url });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+});
+
+// API endpoint for shorturl redirection
+app.get("/api/shorturl/:id", async function (req, res) {
+  const doc = await Url.findOne({ short_url: req.params.id });
+  res.redirect(doc.original_url);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
